c5903cb108f896265ca70653968a5b8e4d887654
AnoaurBENAISSA <anouar.benaissa06@gmail.com>
2026-04-21 21:35:40 +0100

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </div>
         <div className="text-center">
           <h1 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4">
-            sidi yahya on top
+            sidi yahya on top negga
           </h1>
           <p className="text-lg md:text-xl text-gray-600">
             <code className="bg-gray-200 px-2 py-1 rounded">src/App.tsx</code>{" "}
